4d8eb557bcf04134c09a13d67ef7b8ef4fc96082
koraykoca <koraykoca34@gmail.com>
2021-11-18 23:21:31 +0100

    necessary argument for yaml.load() added

--- pcdet/datasets/kitti/kitti_dataset.py
+++ pcdet/datasets/kitti/kitti_dataset.py
@@ -429,7 +429,7 @@ if __name__ == '__main__':
         import yaml
         from pathlib import Path
         from easydict import EasyDict
-        dataset_cfg = EasyDict(yaml.load(open(sys.argv[2])))
+        dataset_cfg = EasyDict(yaml.load(open(sys.argv[2]), Loader=yaml.Loader))
         ROOT_DIR = (Path(__file__).resolve().parent / '../../../').resolve()
         create_kitti_infos(
             dataset_cfg=dataset_cfg,
